a1a0fac8ee1fe47b62526a1c9cb32106a230a74a
sebastian <sebastian.quintero2@utp.edu.co>
2025-05-25 13:15:32 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { LandingComponent } from "./landing/landing.component";
+import { AboutComponent } from "./about/about.component";
+import { BlogComponent } from "./blog/blog.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FooterComponent, LandingComponent, AboutComponent, BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
